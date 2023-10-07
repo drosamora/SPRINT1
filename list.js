6508a4bd7e0e9752087c7b000a38ebe083af7675
drosamora <moradenia9@gmail.com>
2023-10-07 16:38:44 -0500

    se agrega el proyecto

---
+++ list.js
@@ -0,0 +1,13 @@
+let listaProductos = [
+{
+    name: "Zapatillas de Mujer",
+    Precio: 50000
+    Descripcion: "Zapatillas rojas",
+    img: "Mujer.png",
+},
+{
+    name: "Zapatos de Hombres",
+    Precio: 50000
+    Descripcion: "Zapatos Para Hombres",
+    img: "Hombres.png",
+}
